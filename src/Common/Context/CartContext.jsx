9ea16f671a3e2571856353fd712df587e8d11f87
Muhsina-P-M <pmmhome78@gmail.com>
2025-09-06 22:54:56 +0530

    landingpage, cartpage  and productlistingpage created

---
+++ src/Common/Context/CartContext.jsx
@@ -0,0 +1,27 @@
+import React, { createContext, useState, useContext } from "react";
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  // Add item to cart
+  const addToCart = (product) => {
+  setCartItems([...cartItems, product]); 
+};
+
+
+
+  // Remove item from cart
+  const removeFromCart = (id) => {
+    setCartItems((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
